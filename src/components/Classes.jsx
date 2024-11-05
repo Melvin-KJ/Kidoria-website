4de435fba194ea1356d855ca757de22f7a7e3edf
Melvin-KJ <melvinofficial2001@gmail.com>
2024-11-06 00:52:39 +0530

    optimised code to mobile responsiveness

--- src/components/Classes.jsx
+++ src/components/Classes.jsx
@@ -4,18 +4,18 @@ import '../styles/Classes.css';
 const Classes = () => {
   return (
     <div className="classes">
-      <div className="classes-content">
+      <div className="classes-content ">
         <h2 className="mt-6 text-center lobster">School Classes</h2>
         <p className="mt-2 text-center text-gray-500">
           Explore a diverse range of engaging school classes designed to spark
           curiosity and foster creativity in young minds
         </p>
         <div className="classes-content flex justify-center items-center min-h-screen">
-          <div className="flex flex-wrap justify-center gap-8">
+          <div className="mt-20 flex flex-wrap justify-center gap-6 md:gap-8">
             {/* Card 1 */}
-            <div className="relative bg-orange-100 shadow-lg rounded-lg w-80 h-auto p-6">
+            <div className="mt-36 mb-36 sm:mt-20 sm:mb-0 relative bg-orange-100 shadow-lg rounded-lg w-80 h-auto p-6">
               <div className="absolute -top-32 left-1/2 transform -translate-x-1/2">
-                <div className="w-56 h-56 rounded-full border-8 border-primary hover:border-orange-500 overflow-hidden shadow-md">
+                <div className="w-36 h-36 sm:w-56 sm:h-56 rounded-full border-8 border-primary hover:border-orange-500 overflow-hidden shadow-md">
                   <img
                     src="/assets/classes-1.jpg"
                     alt="art and drawing class"
@@ -23,12 +23,14 @@ const Classes = () => {
                   />
                 </div>
               </div>
-              <div className="mt-20 text-center">
-                <h3 className="text-2xl lobster font-bold">Art & Drawing</h3>
+              <div className="top-space mt-20 text-center">
+                <h3 className="text-xl sm:text-2xl lobster font-bold">
+                  Art & Drawing
+                </h3>
                 <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded-full">
                   ₹7,999
                 </button>
-                <div className="mt-2 flex items-center justify-center gap-6">
+                <div className="mt-2 flex items-center justify-center gap-4 sm:flex-row sm:justify-center sm:gap-6">
                   <div className="mt-2 ">
                     <span className="block text-red-500 font-bold">Age:</span>
                     <span className="block text-gray-600 font-bold">
@@ -56,9 +58,9 @@ const Classes = () => {
             </div>
 
             {/* Card 2 */}
-            <div className="relative bg-orange-100 shadow-lg rounded-lg w-80 h-auto p-6">
+            <div className="mt-36 mb-36 sm:mt-20 sm:mb-0 relative bg-orange-100 shadow-lg rounded-lg w-80 h-auto p-6">
               <div className="absolute -top-32 left-1/2 transform -translate-x-1/2">
-                <div className="w-56 h-56 rounded-full border-8 border-primary hover:border-orange-500 overflow-hidden shadow-md">
+                <div className="w-36 h-36 sm:w-56 sm:h-56 rounded-full border-8 border-primary hover:border-orange-500 overflow-hidden shadow-md">
                   <img
                     src="/assets/classes-3.jpg"
                     alt="art and drawing class"
@@ -66,12 +68,12 @@ const Classes = () => {
                   />
                 </div>
               </div>
-              <div className="mt-20 text-center">
+              <div className="top-space mt-20 text-center">
                 <h3 className="text-2xl lobster font-bold">Athletic & Dance</h3>
                 <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded-full">
                   ₹5,999
                 </button>
-                <div className="mt-2 flex items-center justify-center gap-6">
+                <div className="mt-2 flex items-center justify-center gap-4 sm:flex-row sm:justify-center sm:gap-6">
                   <div className="mt-2 ">
                     <span className="block text-red-500 font-bold">Age:</span>
                     <span className="block text-gray-600 font-bold">
@@ -99,9 +101,9 @@ const Classes = () => {
             </div>
 
             {/* Card 3 */}
-            <div className="relative bg-orange-100 shadow-lg rounded-lg w-80 h-auto p-6">
+            <div className="mt-36 mb-36 sm:mt-20 sm:mb-0 relative bg-orange-100 shadow-lg rounded-lg w-80 h-auto p-6">
               <div className="absolute -top-32 left-1/2 transform -translate-x-1/2">
-                <div className="w-56 h-56 rounded-full border-8 border-primary hover:border-orange-500 overflow-hidden shadow-md">
+                <div className="w-36 h-36 sm:w-56 sm:h-56 rounded-full border-8 border-primary hover:border-orange-500 overflow-hidden shadow-md">
                   <img
                     src="/assets/classes-6.jpg"
                     alt="art and drawing class"
@@ -109,14 +111,14 @@ const Classes = () => {
                   />
                 </div>
               </div>
-              <div className="mt-20 text-center">
+              <div className="top-space mt-20 text-center">
                 <h3 className="text-2xl lobster font-bold">
                   General Knoweledge
                 </h3>
                 <button className="mt-4 bg-orange-500 text-white py-2 px-4 rounded-full">
                   ₹6,999
                 </button>
-                <div className="mt-2 flex items-center justify-center gap-6">
+                <div className="mt-2 flex items-center justify-center gap-4 sm:flex-row sm:justify-center sm:gap-6">
                   <div className="mt-2 ">
                     <span className="block text-red-500 font-bold">Age:</span>
                     <span className="block text-gray-600 font-bold">
